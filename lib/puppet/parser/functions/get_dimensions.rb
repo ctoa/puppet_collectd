968e66243036b218209616d85e682129c1cac059
Christi Toa <ctoa@themeetgroup.com>
2017-07-14 10:39:01 -0700

    Ensure that keys and values are url properly encoded
    Ensure that the keys are values are string else a Fixnum error could be
    thrown

--- lib/puppet/parser/functions/get_dimensions.rb
+++ lib/puppet/parser/functions/get_dimensions.rb
@@ -31,7 +31,7 @@ Puppet::Parser::Functions.newfunction(:get_dimensions, :type => :rvalue) do |arg
     end
 
     unless dimension_list.empty?
-        dimension_list.sort_by { |k| k }.each { |key, value| dimensions << "sfxdim_#{key}=#{value}&" }
+        dimension_list.sort_by { |k| k }.each { |key, value| dimensions << "sfxdim_#{URI.escape(key.to_s)}=#{URI.escape(value.to_s)}&" }
     end
 
     dimensions[0...-1]
